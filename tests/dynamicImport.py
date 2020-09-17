17f20499dac3931276a912df3cd1572a36647700
pidou46 <email_com@laposte.net>
2020-09-17 16:58:46 +0200

    Add files via upload

--- tests/dynamicImport.py
+++ tests/dynamicImport.py
@@ -1,4 +1,18 @@
-moduleName = 'os'
-os = __import__(moduleName)
+moduleName = 'my_module'
+my_module = __import__(moduleName)
 
-print(os.listdir())
+MyClass = getattr(my_module, "Test")
+toto = MyClass()
+
+toto.test()
+
+
+
+
+# You probably don't want to use __import__ to dynamically import a module by name, as it does not allow you to import submodules:
+# 
+# >>> mod = __import__("os.path")
+# >>> mod.join
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# AttributeError: 'module' object has no attribute 'join'
